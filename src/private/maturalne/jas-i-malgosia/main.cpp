f151e2487dedb8a73b0d6ce62ca894ab5b243f80
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2020-09-22 16:46:18 +0200

    code blocks

--- src/private/maturalne/jas-i-malgosia/main.cpp
+++ src/private/maturalne/jas-i-malgosia/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main(){
-    cout << "init";
-
+	int * tab;
+	tab = new int[5];
+	fstream file;
+	file.open("data.txt", ios::in);
+	for(int i = 0 ; i < 5 ; i++){
+		file >> tab[i];
+	}
+	for(int i = 0 ; i < 5 ; i++){
+		cout << tab[i];
+	}
     return 0;
 }
